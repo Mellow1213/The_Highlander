d4837e03930d0d05a100bb8d0aa8b212ceb5e652
Mellow1213 <dpcks7282@gmail.com>
2022-10-15 17:24:56 +0900

    실제 에임 구현 완료
    Raycast

--- Project/Assets/02. Scripts/ThirdPersonShooterController.cs
+++ Project/Assets/02. Scripts/ThirdPersonShooterController.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera _aimVirtualCamera;
     [SerializeField] private float normalSensitivity;
     [SerializeField] private float aimingSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransfrom;
+
     private StarterAssetsInputs _starterAssetsInputs;
     private ThirdPersonController _thirdPersonController;
    
@@ -29,6 +33,13 @@ public class ThirdPersonShooterController : MonoBehaviour
         {
             _aimVirtualCamera.gameObject.SetActive(false);
             _thirdPersonController.SetSensitivity(normalSensitivity);
+        }
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if(Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransfrom.position = raycastHit.point;
         }
     }
 }
