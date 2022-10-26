177216930ad74c24cbc7c004d4a0ece775bb253b
Mellow1213 <dpcks7282@gmail.com>
2022-10-26 16:08:56 +0900

    보스 로직 및 navmesh 추가

--- Project/Assets/02. Scripts/Enemy/Boss_Attack.cs
+++ Project/Assets/02. Scripts/Enemy/Boss_Attack.cs
@@ -5,38 +5,77 @@ using UnityEngine;
 public class Boss_Attack : MonoBehaviour
 {
     [SerializeField] int phase = 1;
-
-    public GameObject servant;
-    float health;
+    [SerializeField] float health;
+    [SerializeField] bool death = false;
+    Animator _animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch (phase)
+        if (!death)
+            switch (phase)
+            {
+                case 1:
+                    Phase1();
+                    break;
+                case 2:
+                    Phase2();
+                    break;
+                case 3:
+                    Phase3();
+                    break;
+                default:
+                    Debug.Log("끝");
+                    break;
+            }
+        else if(phase <= 3)
         {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            default:
-                Debug.Log("끝");
-                break;
+            phase++;
+            death = false;
+        }
+        else
+        {
+            Death();
         }
     }
 
+    void Death()
+    {
+        Debug.Log("최종 사망");
+    }
+
+    void EndGame()
+    {
+        Debug.Log("플레이어 패배");
+    }
+
+    public GameObject muzzle;
+    public GameObject bullet;
+    public float fireDelay;
+    float fireTimer = 0f;
+    public GameObject explosion;
+    public GameObject laser;
+    public float skillDelay;
+    float skillTimer = 0;
+
+    bool phase1Start = false;
     void Phase1()
     {
         //총알 공격
         //가끔 폭발 or 메테오(레이저). 시전 직전에 바닥 이펙트
     }
+
+    public GameObject barrier;
+    public float barrierSpawnDelay;
+    float barrierSpawnTimer = 0f;
+    public GameObject servant;
+    bool phase2Start = false;
 
     void Phase2()
     {
@@ -45,8 +84,20 @@ public class Boss_Attack : MonoBehaviour
         //일정 시간마다 서번트 소환
     }
 
+    public GameObject flyServant;
+    public Transform phase3Position;
+    bool allowAttacked = false;
+
+    public float endGameDelay;
+    float endGameTimer = 0f;
+    bool phase3Start = false;
     void Phase3()
     {
+        endGameTimer += Time.deltaTime;
+        if (endGameTimer >= endGameDelay)
+        {
+            EndGame();
+        }
         //공중에 서번트 3마리 소환
         //잠깐 사라졌다가 코어 위 공중에 위치
         //서번트 3마리 잡아야 공격 가능
