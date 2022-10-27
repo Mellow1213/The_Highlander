b09d8bb06875f331ca8095e3f367f835010c1e26
Mellow1213 <dpcks7282@gmail.com>
2022-10-28 01:03:30 +0900

    dissolve 이펙트 추가 & 안쓰는 에셋 삭제

--- Project/Assets/02. Scripts/SpawnParticle.cs
+++ Project/Assets/02. Scripts/SpawnParticle.cs
@@ -5,17 +5,4 @@ using UnityEngine.VFX;
 
 public class SpawnParticle : MonoBehaviour
 {
-    public VisualEffect magicBall;
-    public VisualEffect Snow;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        magicBall.pause = true;
-    }
 }
