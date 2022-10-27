7763e6e47169fed6d3f68dd82514d2d21f903af7
Mellow1213 <dpcks7282@gmail.com>
2022-10-28 03:07:51 +0900

    메뉴씬 작업 중 및 메인씬 줌 함수 변경

---
+++ Project/Assets/MainMenuPose.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuPose : MonoBehaviour
+{
+    public int poseNum;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetInteger("Pose", poseNum);
+
+    }
+
+}
